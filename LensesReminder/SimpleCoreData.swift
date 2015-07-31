032766aaf965a4b17b21814b5d8d56e44370aa21
Adam Nowak <adamcbf@gmail.com>
2015-07-31 16:19:40 +0200

    notification update

---
+++ LensesReminder/SimpleCoreData.swift
@@ -0,0 +1,35 @@
+//
+//  SimpleCoreData.swift
+//  LensesReminder
+//
+//  Created by Adam Nowak on 31.07.2015.
+//  Copyright (c) 2015 Nowak Adam. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCoreData: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
